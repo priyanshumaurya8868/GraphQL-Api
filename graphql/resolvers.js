de99660abf90182a968fac8bf52aa0456db54d35
Priyanshu <priyanshumaurya.8868@gmail.com>
2022-08-18 10:46:22 +0530

    ft/user input vaidation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,30 @@
 const bcrypt = require("bcryptjs");
-
+const validator = require('validator')
 const User = require("../models/User");
 
 module.exports = {
   //here always be 2 prams, args can contain more than one object which were passec
   createUser: async function (args, req) {
     const userInput = args.userInput;
+
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: 'E-Mail is invalid.' });
+    }
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: 'Password too short!' });
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+
+
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
       throw new Error("User exists already!");
