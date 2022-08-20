c095f996b8a1b685375fe9838f69bf9cb4b5a3b1
Priyanshu <priyanshumaurya.8868@gmail.com>
2022-08-20 18:00:23 +0530

    crud

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,60 +1,64 @@
-const { buildSchema } = require("graphql");
+const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-type Post{
-    _id : ID!
-    title : String!
-    content : String!
-    imageUrl : String!
-    creator : User!
-    createdAt : String!
-    updatedAt : String!
-}
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
+    }
 
-type User {
- _id : ID!
- name : String!
- status : String!
- password : String!
- email : String!
- post : [Post!]!
-}
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+        posts: [Post!]!
+    }
 
-input UserInputData{
- name : String!
- email : String!
- password :String!
- }
+    type AuthData {
+        token: String!
+        userId: String!
+    }
 
-input PostInputData{
-    title : String!
-    imageUrl : String!
-    content : String!
-}
+    type PostData {
+        posts: [Post!]!
+        totalPosts: Int!
+    }
 
-type RootMutation {
-createUser(userInput : UserInputData) : User!
-createPost(postInput : PostInputData) : Post!
-}
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
 
-type UserData{
-    token : String!
-    userId : String!
-}
-type PostData {
-    posts: [Post!]!
-    totalPosts: Int!
-}
+    input PostInputData {
+        title: String!
+        content: String!
+        imageUrl: String!
+    }
 
-type RootQuery{
- login(email: String! password : String!) : UserData!
- posts(page: Int): PostData! 
- post(id : ID!):Post
- }
+    type RootQuery {
+        login(email: String!, password: String!): AuthData!
+        posts(page: Int): PostData!
+        post(id: ID!): Post!
+        user: User!
+    }
 
-schema {
-query : RootQuery
-mutation: RootMutation
-}
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+        createPost(postInput: PostInputData): Post!
+        updatePost(id: ID!, postInput: PostInputData): Post!
+        deletePost(id: ID!): Boolean
+        updateStatus(status: String!): User!
+    }
 
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
 `);
