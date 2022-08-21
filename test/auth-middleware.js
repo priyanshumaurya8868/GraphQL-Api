a64461d4a9e90536af418954c8c0d354d468b786
Priyanshu <priyanshumaurya.8868@gmail.com>
2022-08-21 20:35:16 +0530

    how to organize test cases with describe()

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,13 +1,29 @@
 const expect = require('chai').expect
 const auth_middleware = require('../middleware/is-auth')
 
-it('should throw error if Authourization header is not present.',()=>{
-       const req = {
-        get :(headername)=>{
-            return null
+
+//use describe() to organize ur test cases 
+describe('Testing Auth-Middleware',()=>{
+
+    //u can even have multiple describe() inside descibe(),i.e. nesting...
+
+    it('should throw error if Authourization header is not present.',()=>{
+        const req = {
+         get :(headername)=>{
+             return null
+         }
         }
-       }
-
-       expect(auth_middleware.bind(this,req,{},()=>{})).to.throw('Not authenticated.')
-
+        expect(auth_middleware.bind(this,req,{},()=>{})).to.throw('Not authenticated.')
+ 
+ })
+ 
+ it('should throw error if auth header could not split into 2 parts',()=>{
+     const req = {
+         get :(headername)=>{
+             return 'bearer'
+         }
+        }
+          //we can just use throw() alone, when we dont know the error message 
+        expect(auth_middleware.bind(this,req,{},()=>{})).to.throw()
+ })
 })
